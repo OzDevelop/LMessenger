8d89ce2990f5033cd05f58a5de83f4b82acf6772
byunsp3610@gmail.com <byunsp3610@gmail.com>
2023-12-11 18:00:02 +0900

    Feat: LoginIntroView 구성

---
+++ LMessenger/LMessenger/View/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LMessenger
+//
+//  Created by 변상필 on 12/11/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("LoginView")
+    }
+}
+
+#Preview {
+    LoginView()
+}
