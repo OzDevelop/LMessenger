a30b1fb13a57c7f8f77f99fce257abead6d7212c
byunsp3610@gmail.com <byunsp3610@gmail.com>
2023-12-20 11:46:35 +0900

    Feat: PhotosUI 이용한 PhotoPicker 구현

--- LMessenger/LMessenger/Service/Services.swift
+++ LMessenger/LMessenger/Service/Services.swift
@@ -11,17 +11,20 @@ protocol ServiceType {
     var authService: AuthenticationServiceType { get set }
     var userService: UserServiceType { get set }
     var contactService: ContactServiceType { get set }
+    var photoPickerService: PhotoPickerServiceType { get set }
 }
 
 class Services: ServiceType {
     var userService: UserServiceType
     var authService: AuthenticationServiceType
     var contactService: ContactServiceType
+    var photoPickerService: PhotoPickerServiceType
     
     init() {
         self.authService = AuthenticationService()
         self.userService = UserService(dbRepository: UserDBRepository())
         self.contactService = ContactService()
+        self.photoPickerService = PhotoPickerService()
     }
 }
 
@@ -29,4 +32,5 @@ class StubService: ServiceType {
     var authService: AuthenticationServiceType = StubAuthenticationService()
     var userService: UserServiceType = StubUserService()
     var contactService: ContactServiceType = ContactService()
+    var photoPickerService: PhotoPickerServiceType = PhotoPickerService()
 }
--- LMessenger/LMessenger/View/Profile/MyProfileViewModel.swift
+++ LMessenger/LMessenger/View/Profile/MyProfileViewModel.swift
@@ -6,12 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+import PhotosUI
 
 @MainActor // 해당 값들이 메인 액터에서 액세스됨.
 class MyProfileViewModel: ObservableObject {
     
     @Published var userInfo: User? // 자신에 대한 user 정보
     @Published var isPresentedDescEditView: Bool = false
+    @Published var imageSelection: PhotosPickerItem? {
+        didSet {
+            Task {
+                await updateProfileImage(pickerItem:imageSelection)
+            }
+        }
+    }
     
     private let userId: String
     private var container: DIContainer
@@ -31,6 +40,17 @@ class MyProfileViewModel: ObservableObject {
         do {
             try await container.services.userService.updateDescription(userId: userId, description: description)
             userInfo?.description = description
+        } catch {
+              
+        }
+    }
+    
+    func updateProfileImage(pickerItem: PhotosPickerItem?) async {
+        guard let pickerItem else { return}
+        do {
+            let data = try await container.services.photoPickerService.loadTransdferable(from: pickerItem)
+            // 이 데이터를 이용해서 스토리지에 업로드 예정
+            // 이 정보를 db에 업데이트
         } catch {
             
         }
